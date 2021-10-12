c729a74d627daa5db5d02eef6ae3bebd335b9af7
MVas1699 <63935172+MVas1699@users.noreply.github.com>
2021-10-12 12:26:53 -0500

    Add files via upload

---
+++ Tokens.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package p1_lfa;
+
+/**
+ *
+ * @author migue
+ */
+public enum Tokens {
+    Reservadas,
+    Positivo,
+    Negativo,
+    Entero,
+    Decimal,
+    CadenaNoValida,
+    NoValido,
+    ErrorMatematico,
+    ERROR
+}
